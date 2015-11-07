1d48a2405f7bf75f127f8958ac049fbc8f0470ab
Iain R. Learmonth <irl@debian.org>
2015-11-07 11:01:55 +0000

    vmdebootstrap helper expanded

--- lbng/vm.py
+++ lbng/vm.py
@@ -8,6 +8,23 @@ lbng/vm.py - vmdebootstrap helpers
 
 from vmdebootstrap.base import runcmd
 
+class VMDebootstrap:
+
+    def __init__(distribution, mirror=None):
+        self.args = ["vmdebootstrap",
+            "--sudo", "--lock-root-password",
+            "--enable-dhcp", "--configure-apt", "--log", "vmdebootstrap.log"
+            "--squash=cdroot/live/", "--log-level", "debug",
+            "--customize", "hooks/customise.sh"]
+
+        self.args.extend(["--distribution", self.settings['distribution']])
+
+        if mirror is not None:
+            self.args.extend(["--mirror", self.settings['mirror'])
+
+    def run(self):
+        runcmd(args)
+
 def detect_kernels():
     versions = []
     filenames = os.listdir(directory)
@@ -15,10 +32,3 @@ def detect_kernels():
         if filename[0:8] == "vmlinuz-":
             versions.append(filename[8:])
     return versions
-
-def vmdebootstrap(args):
-    """
-    Call vmdebootstrap.
-    """
-    runcmd(args)
-
