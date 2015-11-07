c381a32d318e3a1009eb415e265b0117f3f30c0b
Iain R. Learmonth <irl@debian.org>
2015-11-07 23:08:02 +0000

    I'll tidy this up later

--- hooks/customise.sh
+++ hooks/customise.sh
@@ -13,7 +13,7 @@ mount_support
 disable_daemons
 # prepare_apt_source
 
-chroot ${rootdir} apt-get -y install initramfs-tools live-boot live-config ${LBNG_TASK_PACKAGES} ${LBNG_EXTRA_PACKAGES}
+chroot ${rootdir} apt-get -y install initramfs-tools live-boot live-config ${LBNG_TASK_PACKAGES} ${LBNG_EXTRA_PACKAGES} task-laptop task-english
 
 echo "blacklist bochs-drm" > $rootdir/etc/modprobe.d/qemu-blacklist.conf
 
