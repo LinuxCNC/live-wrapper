b3fe56dc1c47df36517d7e91ede1d1e239d628f9
Iain R. Learmonth <irl@debian.org>
2015-12-18 13:56:55 +0000

    Pass mirror information to vmdebootstrap hook

--- hooks/customise.sh
+++ hooks/customise.sh
@@ -11,9 +11,11 @@ trap cleanup 0
 
 mount_support
 disable_daemons
-# prepare_apt_source
+prepare_apt_source
 
 chroot ${rootdir} apt-get -y install initramfs-tools live-boot live-config ${LBNG_TASK_PACKAGES} ${LBNG_EXTRA_PACKAGES} task-laptop task-english
 
 echo "blacklist bochs-drm" > $rootdir/etc/modprobe.d/qemu-blacklist.conf
+
+replace_apt_source
 
